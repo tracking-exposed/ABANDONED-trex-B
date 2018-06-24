3141eb884563aaf3f2c2f68869f22a9e13c5720c
Christo <christo@cryptodrunks.net>
2018-06-24 10:42:14 +0200

    refactor(processor-cli): append js to bin filename

--- packages/processor-cli/bin/processorctl.js
+++ packages/processor-cli/bin/processorctl.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
+const {run} = require("..");
 
 process.on("unhandledRejection", (e) => {
   // eslint-disable-next-line no-console
   console.error(e);
   process.exit(1);
 });
-
-const {run} = require("..");
 
 run();
