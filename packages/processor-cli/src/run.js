d3143c3cc699db8e82bf8b3a89a34b128b419a0c
Christo <christo@cryptodrunks.net>
2018-06-25 19:35:57 +0200

    feat(processor-cli): register handlers on shutdown

--- packages/processor-cli/src/run.js
+++ packages/processor-cli/src/run.js
@@ -4,10 +4,32 @@ import loader from "./loader";
 import {client, pollFromStream} from "./redis";
 import {runForever} from "./loop";
 
+const registerShutdown = (fn: () => mixed): void => {
+  let run = false;
+  const wrapper = () => {
+    if (!run) {
+      run = true;
+      fn();
+    }
+  };
+  process.on("SIGINT", () => {
+    wrapper();
+    process.exit(128);
+  });
+  process.on("SIGTERM", () => {
+    wrapper();
+    process.exit(128);
+  });
+  process.on("exit", wrapper);
+};
+
 export default async (opts: {[string]: mixed} = {}) => {
   const cfg = Object.assign(cli().parse(), opts);
   const redis = client();
   const processor = await loader(cfg.processor);
+
+  registerShutdown(() => console.log("Shutting down."));
+  console.log("Starting to poll events.");
 
   runForever(async () => {
     const events = await pollFromStream(cfg.stream, redis);
