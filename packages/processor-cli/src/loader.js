ed8b88adc1faa7d2cc39bc3e07392261e9115325
Christo <christo@cryptodrunks.net>
2018-06-25 22:06:47 +0200

    refactor(processor-cli): improved processor module loading

--- packages/processor-cli/src/loader.js
+++ packages/processor-cli/src/loader.js
@@ -2,7 +2,9 @@
 /* eslint global-require: off, import/no-dynamic-require: off */
 import path from "path";
 
-export default (pkg: string): Promise<() => mixed> => {
+import type {StreamProcessor} from "./types";
+
+export default (pkg: string): Promise<StreamProcessor> => {
   const loader = new Promise((resolve, reject) => {
     // If pkg is an absolute path, path,join automatically drops the path from
     // process.cwd().
@@ -15,9 +17,11 @@ export default (pkg: string): Promise<() => mixed> => {
       // $FlowFixMe
       mod = require(pkgPath);
     } catch (e) {
-      return reject(new Error(`Failed to load the module at ${pkgPath}.`));
+      const reason = new Error(`Failed to load the module at ${pkgPath}.`);
+      reason.stack += `\nCaused By:\n${e.stack}`;
+      return reject(reason);
     }
-    return resolve(mod);
+    return resolve(mod.default ? mod.default : mod);
   });
   return loader;
 };
