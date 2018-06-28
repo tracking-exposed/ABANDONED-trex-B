618e9a438fb303890b6a4ad467c08bbfae0c002f
Christo <christo@cryptodrunks.net>
2018-06-28 23:21:15 +0200

    chore: load html fixtures with the impressions fixtures

--- .scripts/load-mongo-fixtures.js
+++ .scripts/load-mongo-fixtures.js
@@ -5,8 +5,13 @@ const {MongoClient} = require("mongodb");
 
 const impressions = require(path.join(
   process.cwd(),
-  "packages/processor-cli/test/fixtures/impressions",
+  "packages/data/test/fixtures/impressions",
 ));
+const htmls = require(path.join(
+  process.cwd(),
+  "packages/data/test/fixtures/htmls",
+));
+
 
 dotenv.config();
 
@@ -20,7 +25,13 @@ const mongoUri = `mongodb://${mongoHost}:${mongoPort}/${mongoDb}`;
   const mongo = await MongoClient.connect(mongoUri);
   const db = mongo.db();
   const collections = await db.collections();
-  if (collections.length > 0) await db.dropCollection("impressions");
+  if (collections.length > 0) {
+    try {
+      await db.dropCollection("impressions");
+      await db.dropCollection("htmls");
+    } catch (e) {}
+  }
   await db.collection("impressions").insert(impressions);
+  await db.collection("htmls").insert(htmls);
   process.exit(0);
 })();
