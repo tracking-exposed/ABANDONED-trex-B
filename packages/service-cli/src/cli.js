b302995c229864439ba9e5ec84f46db042ebe794
Christo <christo@cryptodrunks.net>
2018-07-01 19:16:54 +0200

    feat(service-cli): start micro services using servicectl

--- packages/service-cli/src/cli.js
+++ packages/service-cli/src/cli.js
@@ -4,15 +4,30 @@ import findUp from "find-up";
 import fs from "fs";
 
 export default () => {
-  const configPath = findUp.sync([".processorrc", ".processor.json"]);
+  const configPath = findUp.sync([".servicerc", ".service.json"]);
   const config = configPath
     ? JSON.parse(fs.readFileSync(configPath).toString())
     : {};
 
   return yargs
+    .command("start", "Start up a HTTP service", (y) =>
+      y
+        .option("port", {
+          default: 3000,
+          type: "number",
+          desc: "The port to listen on.",
+        })
+        .alias("port", "p")
+        .option("service", {
+          required: true,
+          type: "string",
+          desc: "Run a service using this service handler.",
+        })
+        .alias("service", "s"),
+    )
     .config(config)
     .env("TREX")
-    .pkgConf("processor")
+    .pkgConf("service")
     .demandCommand()
     .help()
     .wrap(72);
--- packages/service-cli/src/run.js
+++ packages/service-cli/src/run.js
@@ -1,10 +1,21 @@
 // @flow
+import {registerShutdown, loadPkg} from "@tracking-exposed/utils";
+import micro from "micro";
 import dotenv from "dotenv";
+
 import cli from "./cli";
 
 dotenv.config();
 
 export default async (opts: {[string]: mixed} = {}) => {
   const cfg = Object.assign(cli().parse(), opts);
-  console.log(cfg);
+  const service = await loadPkg(cfg.service);
+
+  // eslint-disable-next-line no-console
+  registerShutdown(() => console.log("Shutting down."));
+  // eslint-disable-next-line no-console
+  console.log("Starting to serve.");
+
+  const server = micro(service);
+  server.listen(cfg.port);
 };
