1c4e3e6a97ba0ddf443fcc4b272757bacb04b926
Christo <christo@cryptodrunks.net>
2018-07-05 13:57:14 +0200

    fix(data): export feeds

--- packages/data/src/index.js
+++ packages/data/src/index.js
@@ -3,3 +3,4 @@ export {default as redis} from "./redis";
 export {default as mongo} from "./mongo";
 export {default as impressions} from "./impressions";
 export {default as entities} from "./entities";
+export {default as feeds} from "./feeds";
